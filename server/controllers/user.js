329f478dd9c10eef89224563cfb3ebe09140e834
codestates <codestates@codestatesui-MacBookPro.local>
2022-04-24 14:10:16 +0900

    controllers 생성

---
+++ server/controllers/user.js
@@ -0,0 +1,10 @@
+module.exports = {
+    user: {
+        get: (req, res) =>{
+            res.send('test');
+        },
+        post: (req, res) => {
+            res.send('test');
+        }
+    }
+}
---
+++ server/controllers/wallet.js
@@ -0,0 +1,7 @@
+module.exports = {
+    wallet: {
+        post: (req, res) => {
+            res.send('test');
+        }
+    }
+}
