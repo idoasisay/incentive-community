d22e2d5f6682570e37224bb7e0c94974a2795e75
idoasisay <ihannah@kakao.com>
2022-04-25 20:54:08 +0900

    유저 지갑을 니모닉 지갑에서 web3.js 지갑으로 변경

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,20 +1,16 @@
 const lightWallet = require('eth-lightwallet');
 const models = require('../models');
 const e = require('express');
+const Web3 = require('web3');
 
-
-// 
-// 함수 앞에 async 예약어 설정
-// http 통신을 하는 비동기 처리 코드 앞에 await
-// 시간 나면 web3.eth.accounts 만들기
 module.exports = {
 
     user: {
         post: async (req, res) => {
-            // 유저 정보 받아오기
+            const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
             const { name, password } = req.body;
-            console.log(name, password);
-            // 모듈화 나중에... => const result = await makeMnemonic(password);
+            // 유저 정보 받아오기
+
             try {
                 models.Users.findOrCreate({
                     where: {
@@ -30,38 +26,17 @@ module.exports = {
                         // 있다고 응답
                         res.status(409).send("User exists");
                     } else {
-                        // 랜덤 시드문구 생성
-                        const mnenmonic = lightWallet.keystore.generateRandomSeed();
-
-                        // 지갑 생성
-                        lightWallet.keystore.createVault(
-                            {
-                                password: password,
-                                seedPhrase: mnenmonic,
-                                hdPathString: "m/0'/0'/0'"
-                            },
-                            function (err, ks) {
-                                ks.keyFromPassword(password, function (err, pwd) {
-                                    ks.generateNewAddress(pwd, 1);
-        
-                                    let address = (ks.getAddresses()).toString();
-                                    let keystore = ks.serialize();
-
-                                    // 유저 업뎃
-                                    models.Users.update({
-                                        address: address,
-                                        privateKey: mnenmonic
-                                    }, {where: {
-                                        userName: name
-                                    }
-                                }).then(result => {
-                                    res.json(address);
-                                }).catch(err => {
-                                    console.error(err);
-                                })
-                                });
-                            }
-                        );
+                        const wallet = web3.eth.accounts.create();
+                        models.Users.update({
+                            address: wallet.address,
+                            privateKey: wallet.privateKey
+                        }, {
+                            where: {userName: name}
+                        }).then(result => {
+                            res.json(wallet.address);
+                        }).catch(err => {
+                            console.error(err);
+                        })
                     }
                 })
             } catch(exception) {
