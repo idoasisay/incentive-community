6a9437c784e36ee6de657a6bc40f8735375271ac
idoasisay <ihannah@kakao.com>
2022-04-25 16:50:37 +0900

    DB 세팅 후 유저 삽입 부분 배열에서 DB로 수정

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,8 @@
 const walletMake = require('./walletHelper');
 const lightWallet = require('eth-lightwallet');
-
-// DB 설정 전까지 임시
 const makeMnemonic = require('./walletHelper');
+const models = require('../models');
+const e = require('express');
 
 
 // 
@@ -15,35 +15,57 @@ module.exports = {
         post: async (req, res) => {
             // 유저 정보 받아오기
             const { name, password } = req.body;
+            console.log(name, password);
             // 모듈화 나중에... => const result = await makeMnemonic(password);
             try {
-                if(database) {
-                    // 아이디가 없으면 무시! 
-                } else {
-                    // 랜덤 시드문구 생성
-                    const mnenmonic = lightWallet.keystore.generateRandomSeed();
-    
-                    // 지갑 생성
-                    lightWallet.keystore.createVault(
-                        {
-                            password: password,
-                            seedPhrase: mnenmonic,
-                            hdPathString: "m/0'/0'/0'"
-                        },
-                        function (err, ks) {
-                            ks.keyFromPassword(password, function (err, pwd) {
-                                ks.generateNewAddress(pwd, 1);
-    
-                                let address = (ks.getAddresses()).toString();
-                                let keystore = ks.serialize();
-    
-                                database[count] = {name, password, address, "privateKey": mnenmonic};
-                                count++;
-                                res.json(address);
-                            });
-                        }
-                    );
-                }
+                models.Users.findOrCreate({
+                    where: {
+                        userName: name,
+                        password: password,
+                    },
+                    defaults: {
+                        address: '',
+                        privateKey: '',
+                    }
+                }).then(([user, created]) => {
+                    if(!created) {
+                        // 있다고 응답
+                        res.status(409).send("User exists");
+                    } else {
+                        // 랜덤 시드문구 생성
+                        const mnenmonic = lightWallet.keystore.generateRandomSeed();
+
+                        // 지갑 생성
+                        lightWallet.keystore.createVault(
+                            {
+                                password: password,
+                                seedPhrase: mnenmonic,
+                                hdPathString: "m/0'/0'/0'"
+                            },
+                            function (err, ks) {
+                                ks.keyFromPassword(password, function (err, pwd) {
+                                    ks.generateNewAddress(pwd, 1);
+        
+                                    let address = (ks.getAddresses()).toString();
+                                    let keystore = ks.serialize();
+
+                                    // 유저 업뎃
+                                    models.Users.update({
+                                        address: address,
+                                        privateKey: mnenmonic
+                                    }, {where: {
+                                        userName: name
+                                    }
+                                }).then(result => {
+                                    res.json(address);
+                                }).catch(err => {
+                                    console.error(err);
+                                })
+                                });
+                            }
+                        );
+                    }
+                })
             } catch(exception) {
                 console.log('error: ', exception);
             }
