caea2f976b713fbe446b84919ab5e49c357cb11e
idoasisay <ihannah@kakao.com>
2022-04-25 22:42:59 +0900

    serverToken api 기본 세팅

---
+++ server/controllers/serverToken.js
@@ -0,0 +1,13 @@
+const lightWallet = require('eth-lightwallet');
+const models = require('../models');
+const e = require('express');
+const Web3 = require('web3');
+
+module.exports = {
+
+    serverToken: {
+        post: async (req, res) => {
+            //TODO
+        }
+    }
+}
--- server/router/index.js
+++ server/router/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const signUpRouter = require('./signUp');
 const ethFaucetRouter = require('./ethFaucet');
+const serverTokenRouter = require('./serverToken');
 
 // TODO: Endpoint에 따라 적절한 Router로 연결해야 합니다.
 router.get('/', (res, req) => req.send('first page test'));
@@ -9,7 +10,7 @@ router.get('/', (res, req) => req.send('first page test'));
 // 으악.. 회원가입 페이지를 만들어야 할 거 같은데...
 router.use('/signUp', signUpRouter);
 router.use('/ethFaucet', ethFaucetRouter);
-
+router.use('/serverToken', serverTokenRouter);
 // 나중에 유저별 처리해 줘야 함
 // router.use('/user:id', userRouter);
 
