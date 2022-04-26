0806289c5e4181102d27f3bfd7d889ad95534c02
idoasisay <ihannah@kakao.com>
2022-04-26 19:50:32 +0900

    요청한 유저에게 1 ICT 토큰을 서버 계정에서 전송하는 API 생성

--- server/controllers/serverToken.js
+++ server/controllers/serverToken.js
@@ -1,25 +1,71 @@
 const lightWallet = require("eth-lightwallet");
 const models = require("../models");
 const e = require("express");
-const Web3 = require("web3");
+const getWeb3 = require("./walletHelper");
+const web3 = getWeb3();
+const {
+  contractABI,
+  contractAddress,
+} = require("../source/tokenInfomation.json");
+
+async function getServerAccount() {
+  return models.Users.findOne({
+    where: { userName: "server" },
+  }).then((data) => data);
+}
 
 module.exports = {
   serverToken: {
     post: async (req, res) => {
-      //TODO
-      // 1. 입력받은 username을 가지고 데이터베이스에 저장되어 있는 주소를 가져옵니다.
       const { name } = req.body;
+      const server = await getServerAccount();
 
-      // 2. 개인키를 사용해 트랜잭션에 서명을 하고
-      // ERC20 컨트랙트의 transfer() 함수를 호출하는 트랜잭션을 보냅니다.
+      models.Users.findOne({ where: { userName: name } }).then((user) => {
+        let contract = new web3.eth.Contract(contractABI, contractAddress, {
+          from: server.address,
+        });
 
-      // 컨트랙트의 transfer() 함수를 사용해 서버 계정에 있는 토큰 1개를 사용자의 주소로 전송합니다.
-      // POST /ethFaucet과 동일한 방식으로 트랜잭션에 서명을 하고 보냅니다.
-      // 컨트랙트에 있는 함수를 실행하기 위해서는 트랜잭션에 data 값을 추가해야 합니다.
-      // https://ethereum.stackexchange.com/questions/95218/how-can-i-transfer-tokens-of-my-erc20-automatically-from-the-server
-      // 컨트랙트의 balanceOf() 함수를 사용하면 특정 주소에 있는 토큰의 갯수를 확인할 수 있습니다.
+        var data = contract.methods
+          .transfer(user.address, "1000000000000000000")
+          .encodeABI();
+        var rawTransaction = { to: contractAddress, gas: 100000, data: data };
 
-      // 3. 트랜잭션의 결과에 따라 응답합니다.
+        web3.eth.accounts
+          .signTransaction(rawTransaction, server.privateKey)
+          .then((signedTx) =>
+            web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+          )
+          .then((req) => {
+            getTOKENBalanceOf(user.address).then((balance) => {
+              res.json({
+                message: "Serving Successed",
+                data: {
+                  username: user.username,
+                  address: user.address,
+                  txHash: req.transactionHash,
+                  tokenBalance: balance,
+                },
+              });
+            });
+          });
+
+        //GET TOKEN BALANCE FUNCTION ////////////////////////////////
+        async function getTOKENBalanceOf(address) {
+          return await contract.methods.balanceOf(address).call();
+        }
+      });
     },
   },
 };
+
+/**
+ * {
+	message: "Serving Successed",
+	data: {
+		username: 'kimcoding',  // 사용자 이름
+		address: '0x32482...',  // 받는 계정의 주소
+		txHash: '0x234C1...'  // 트랜잭션 해시
+		tokenBalance: '12', // 유저의 토큰 잔액
+	}
+}
+ */
