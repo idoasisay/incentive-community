c2d7f38342ef7300d0630cc6a655a539ca4007a5
idoasisay <ihannah@kakao.com>
2022-04-28 01:24:26 +0900

    DB 업데이트: 테이블에 amount 추가, server 계정 seeder 설정, 작성되지 않았던 migration 재설정

--- server/models/index.js
+++ server/models/index.js
@@ -1,36 +1,37 @@
-"use strict";
-// 경로를 위한 내장 모듈
-const path = require("path");
-const Users = require("./Users");
-// sequelize 패키지
-const Sequelize = require("sequelize");
+'use strict';
 
-// 개발 모드 환경설정
-const env = process.env.NODE_ENV || "development";
-// 데이터 베이스 연결 환경설정
-// ../config/config.json에서 해당 env를 키로 가지는 데이터를 가져온다.
-const config = require(path.join(__dirname, "..", "config", "config.json"))[
-  env
-];
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-// 데이터베이스 객체
-const db = {}; // 여기와 실제 데이터베이스가 연결된다.
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-// 데이터베이스 연결정보로 시퀄라이즈 ORM 객체를 생성한다
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
 
-// 데이터베이스와 ORM을 매핑한다.
-// 이제부터 db를 통해 데이터베이스를 관리할 수 있다.
-db.sequelize = sequelize; // db객체에 sequelize라는 프로퍼티 추가
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.Users = Users;
-Users.init(sequelize);
-
-// 만든 db 객체를 exports 한다.
 module.exports = db;
