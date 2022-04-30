1ab1241e276f0c3dae130aa3a66161f8d169aa61
idoasisay <ihannah@kakao.com>
2022-04-30 22:31:25 +0900

    업데이트: Web3 생성 함수 정리

--- server/controllers/walletHelper.js
+++ server/controllers/walletHelper.js
@@ -1,5 +1,15 @@
 const Web3 = require("web3");
 
+const httpWeb3 = function (host) {
+  const web3 = new Web3(new Web3.providers.HttpProvider(host));
+  return web3;
+};
+
+const soketWeb3 = function (host) {
+  const web3 = new Web3(new Web3.providers.WebsocketProvider(host));
+  return web3;
+};
+
 function getWeb3() {
   const web3 = new Web3(
     new Web3.providers.HttpProvider("http://127.0.0.1:7545")
@@ -7,4 +17,6 @@ function getWeb3() {
   return web3;
 }
 
-module.exports = getWeb3;
+exports.httpWeb3 = httpWeb3;
+exports.soketWeb3 = soketWeb3;
+exports.getWeb3 = getWeb3;
