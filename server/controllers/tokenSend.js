ce3bf9cd17d6200d30edc1c68cabf5807e4fcdc8
idoasisay <ihannah@kakao.com>
2022-05-04 23:06:45 +0900

    생성: 유저끼리 토큰을 주고받을 수 있는 API

--- server/controllers/tokenSend.js
+++ server/controllers/tokenSend.js
@@ -4,43 +4,60 @@ const {
   contractABI,
   contractAddress,
 } = require("../source/tokenInfomation.json");
-
-const web3 = web3Helper.getWeb3();
+const Web3 = require("web3");
+const web3 = new Web3(
+  new Web3.providers.WebsocketProvider("ws://127.0.0.1:7545")
+);
 
 module.exports = {
   tokenSend: {
-    post: async (req, res) => {},
-  },
-};
-
-// 토큰 진짜로 전송하는 건 이쪽
-/*
-// "1000000000000000000"
+    post: async (req, res) => {
       const { fromUserName, toUserName, amount } = req.body;
       // from 유저 -> to 유저 전송
-      const fromUser = await User.findOne({where: { userName: fromUserName }});
+      const fromUser = await User.findOne({
+        where: { userName: fromUserName },
+      });
       const toUser = await User.findOne({ where: { userName: toUserName } });
 
-      if(!fromUser || !toUser) res.status(409).send('유저가 없습니다.');
+      if (!fromUser || !toUser) res.status(409).send("유저가 없습니다.");
 
       const options = {
         from: fromUser.dataValues.address,
         gasPrice: 100,
-        gas: 100000
-      }
-
+        gas: 100000,
+      };
       // 콘트랙트 받기
       let contract = new web3.eth.Contract(contractABI, contractAddress);
-      // web3.eth.personal.newAccount(password, [callback])
-      web3.eth.personal.unlockAccount(fromUser.dataValues.address, fromUser.dataValues.password, 600).then(console.log('Account unlocked!'))
+      const fromaddress = await fromUser.dataValues.address;
+      const frompassword = await fromUser.dataValues.password;
+
+      await web3.eth.personal
+        .unlockAccount(fromaddress, frompassword, 600)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
 
       const data = contract.methods.transfer(toUser.dataValues.address, amount);
-      data.send(options, (err, tx)=> {
-        if(err) console.log(err);
-        console.log(tx);
-      })
-      res.send('');
-        async function getTOKENBalanceOf(address) {
-          return await contract.methods.balanceOf(address).call();
-        }
- */
+      data.send(options, async (err, tx) => {
+        if (err) console.log(err);
+        res.status(201).json({
+          message: "토큰을 성공적으로 보냈습니다.",
+          data: {
+            transaction: tx,
+            from: fromUserName,
+            to: toUserName,
+            amount: amount,
+            balance: await getTOKENBalanceOf(fromaddress),
+          },
+        });
+      });
+      // res.end();
+      async function getTOKENBalanceOf(address) {
+        return await contract.methods.balanceOf(address).call();
+      }
+    },
+  },
+};
