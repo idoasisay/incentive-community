019d60dbcf377eca47f52896a979b7cce8da3e8e
idoasisay <ihannah@kakao.com>
2022-05-05 01:52:59 +0900

    업데이트: ERC721에 따른 토큰 이동 추가

--- server/controllers/watcher.js
+++ server/controllers/watcher.js
@@ -2,6 +2,8 @@ const { User } = require("../models");
 const {
   contractABI,
   contractAddress,
+  erc721ABI,
+  erc721Address,
 } = require("../source/tokenInfomation.json");
 const soketWeb3 = require("./walletHelper");
 const Web3 = require("web3");
@@ -23,8 +25,16 @@ function watchEtherTransfers() {
       const { from, to } = trx;
 
       // 토큰이라면...
-      if (to === contractAddress) watchTokenTransfers();
-      else {
+      if (to === contractAddress)
+        watch20Transfers(contractAddress, contractABI);
+      else if (to === erc721Address) {
+        watch721Transfers(
+          erc721Address,
+          erc721ABI,
+          contractAddress,
+          contractABI
+        );
+      } else {
         // 아니라면...
         const addresses = [from, to];
 
@@ -41,8 +51,8 @@ function watchEtherTransfers() {
 }
 
 // 토큰 트잭
-function watchTokenTransfers() {
-  const tokenContract = new web3.eth.Contract(contractABI, contractAddress);
+function watch20Transfers(CA, ABI) {
+  const tokenContract = new web3.eth.Contract(ABI, CA);
   tokenContract.events.Transfer({}, async (err, event) => {
     console.log("smart contract event log: ", event);
     // 에러 처리
@@ -55,6 +65,32 @@ function watchTokenTransfers() {
 
     addresses.forEach(async (e) => {
       let amount = await tokenContract.methods.balanceOf(e).call();
+      const user = await User.findOne({ where: { address: e } });
+      // User DB 업데이트
+      if (user !== null)
+        User.update({ tokenAmount: amount }, { where: { address: e } });
+    });
+    console.log("------------------------------------------------------");
+  });
+}
+
+// 여기엔 20 트잭 따로 가지고 와야 하네요 ;
+async function watch721Transfers(CA721, ABI721, CA20, ABI20) {
+  const tokenContract = new web3.eth.Contract(ABI721, CA721);
+  const contract20 = new web3.eth.Contract(ABI20, CA20);
+  const serverAddress = await User.findOne({ where: { userName: "server" } });
+
+  tokenContract.events.Transfer({}, async (err, event) => {
+    console.log("smart contract event log: ", event);
+    // 에러 처리
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const addresses = [serverAddress.dataValues.address, event.returnValues.to];
+
+    addresses.forEach(async (e) => {
+      let amount = await contract20.methods.balanceOf(e).call();
       const user = await User.findOne({ where: { address: e } });
       // User DB 업데이트
       if (user !== null)
